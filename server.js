200f436886575279bd46227cae2e94d861a4690f
Sam Rodriguez <samrodriguez826@gmail.com>
2019-12-13 15:32:11 -0500

    basic html pages up

--- server.js
+++ server.js
@@ -1,26 +1,20 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routing/htmlRoutes');
-var usersRouter = require('./routing/apiRoutes');
+var htmlRouter = require('./app/routing/htmlRoutes');
+var apiRouter = require('./app/routing/apiRoutes');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', htmlRouter);
+app.use('/users', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
